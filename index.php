7f51ff30d443806d447fab2983b4e90fd82d8195
hkainth7 <harjit.kainth7@gmail.com>
2022-06-22 09:43:17 -0700

    changes to html structure and added css file styles.css with basic sytles

--- index.php
+++ index.php
@@ -5,63 +5,69 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Process</title>
+    <link rel="stylesheet" href="./styles.css">
 </head>
 <body>
-    <div>
+    <div id="wrapper">
         <form action="./form_process.php" method="post">
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username">
+            <fieldset class="user-info">
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+    
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+    
+                <label for="student_number">Student Number</label>
+                <input type="text" name="student_number" id="student_number" placeholder="A0nnnnnnn">
 
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-
-            <label for="student_number">Student Number</label>
-            <input type="text" name="student_number" id="student_number" placeholder="A0nnnnnnn">
-
-            <label for="gender">Gender</label>
+                <div class="gender-info">
+                    <h3>Gender</h3>
+                
+                    <label for="male">Male</label>
+                    <input type="radio" name="gender" id="gender_male" value="male">
         
-            <input type="radio" name="gender" id="gender_male" value="male">
-            <label for="male">Male</label>
-
-            <input type="radio" name="gender" id="gender_female" value="female">
-            <label for="female">Female</label>
-
-            <input type="radio" name="gender" id="gender_not_listed" value="not_listed">
-            <label for="not_listed">Not Listed</label>
-
-            <label for="programming_languages">Studying any computer languages?</label>
-            
-            <input type="checkbox" name="programming_languages[]" id="" value="javascript">
-            <label for="javascript">JavaScript</label>
-
-            <input type="checkbox" name="programming_languages[]" id="" value="c++">
-            <label for="c++">C++</label>
-
-            <input type="checkbox" name="programming_languages[]" id="" value="java">
-            <label for="java">Java</label>
-
-            <input type="checkbox" name="programming_languages[]" id="" value="python">
-            <label for="python">Python</label>
-
-            <input type="checkbox" name="programming_languages[]" id="" value="c#">
-            <label for="c#">C#</label>
-
-            <input type="checkbox" name="programming_languages[]" id="" value="ruby">
-            <label for="ruby">Ruby</label>
+                    <label for="female">Female</label>
+                    <input type="radio" name="gender" id="gender_female" value="female">
+        
+                    <label for="not_listed">Not Listed</label>
+                    <input type="radio" name="gender" id="gender_not_listed" value="not_listed">
+                </div>
+            </fieldset>
+            <fieldset class="lang_info">
+                <div class="programming-langs">
+                    <h3>Studying any computer languages?</h3>
+                    <label for="javascript">JavaScript</label>
+                    <input type="checkbox" name="programming_languages[]" id="" value="javascript">
+        
+                    <label for="c++">C++</label>
+                    <input type="checkbox" name="programming_languages[]" id="" value="c++">
+        
+                    <label for="java">Java</label>
+                    <input type="checkbox" name="programming_languages[]" id="" value="java">
+        
+                    <label for="python">Python</label>
+                    <input type="checkbox" name="programming_languages[]" id="" value="python">
+        
+                    <label for="c#">C#</label>
+                    <input type="checkbox" name="programming_languages[]" id="" value="c#">
+        
+                    <label for="ruby">Ruby</label>
+                    <input type="checkbox" name="programming_languages[]" id="" value="ruby">
+                </div>
+            </fieldset>
 
             <input type="submit" value="Submit">
         </form>
-        
-    </div>
-    <?php 
-        session_start();
-        
 
-        if(isset($_SESSION['error'])) {
-            echo $_SESSION['error'];
-            unset($_SESSION['error']);
-        }
-        
-    ?>
+        <?php 
+            session_start();
+
+            if(isset($_SESSION['error'])) {
+                echo $_SESSION['error'];
+                unset($_SESSION['error']);
+            }            
+        ?>
+    </div>
+    
 </body>
 </html>
